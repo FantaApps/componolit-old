85ea49e0ed151493a52769f96893ae37b615ad33
Alexander Senier <senier@componolit.com>
2018-03-03 00:55:16 +0100

    Remove libfakeatomic
    Replaced by compiler intrinsics in libart.

--- lib/mk/libfakeatomic.mk
+++
@@ -1,6 +0,0 @@
-include $(call select_from_repositories,lib/import/import-libfakeatomic.mk)
-vpath % $(call select_from_repositories,src/lib/libfakeatomic)
-
-LIBS += stdcxx
-SRC_CC += fakeatomic.cc
-SHARED_LIB = yes
