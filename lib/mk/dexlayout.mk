53b198a08cbad97ba40a6a65971c3660888b843d
Alexander Senier <senier@componolit.com>
2018-03-12 00:18:57 +0100

    Create library version of dexlayout
    Closes #90

---
+++ lib/mk/dexlayout.mk
@@ -0,0 +1,10 @@
+ANDROID_PORT = libart
+ANDROID_DIR = art
+ANDROID_SOURCES = dexlayout
+ANDROID_BUILDFILES = Android.bp test/Android.bp build/Android.bp dexlayout/Android.bp
+ANDROID_SECTIONS = /art_cc_binary[@name=dexlayout]
+
+include $(call select_from_repositories,lib/mk/libart-defaults.inc)
+include $(call select_from_repositories,lib/mk/android-lib.inc)
+
+LIBS += libnativehelper libart-dexlayout
---
+++ lib/mk/libz.mk
@@ -0,0 +1,2 @@
+# zlib is called lib libz in Android dependencies. Just include it here.
+include $(call select_from_repositories,lib/mk/zlib.mk)
