6685cbf84c336c8f07e4875c7354350cddcad887
Alexander Senier <senier@componolit.com>
2018-03-12 23:34:55 +0100

    Create library version of unzip
    Closes #94

---
+++ lib/mk/unzip.mk
@@ -0,0 +1,28 @@
+UNZIP_DIR = $(call select_from_ports,unzip)/unzip
+
+SRC_C = \
+	crc32.c \
+	crypt.c \
+	envargs.c \
+	explode.c \
+	extract.c \
+	fileio.c \
+	globals.c \
+	inflate.c \
+	list.c \
+	match.c \
+	process.c \
+	ttyio.c \
+	unix/unix.c \
+	unshrink.c \
+	unzip.c \
+	zipinfo.c \
+
+INC_DIR += $(call select_from_repositories,src/lib/test/unzip)
+INC_DIR += $(UNZIP_DIR)
+vpath %.c $(UNZIP_DIR) $(UNZIP_DIR)/unix
+
+CC_OPT += -DNO_LCHMOD -DNO_LCHOWN
+
+LIBS += libc
+SHARED_LIB = yes
