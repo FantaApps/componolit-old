68f09c1b6cf9ce0de85870d30357674e24c0c232
Alexander Senier <senier@componolit.com>
2018-03-16 11:10:00 +0100

    Make write exit via exit()

--- src/test/write/write.c
+++ src/test/write/write.c
@@ -14,23 +14,23 @@ main (int argc, char **argv)
     if (argc != 3)
     {
         printf ("Invalid arguments: argc==%d\n", argc);
-        return 1;
+        exit(1);
     }
 
     fd = open(argv[1], O_RDWR|O_CREAT);
     if (fd < 0)
     {
         printf ("Error opening %s: %s\n", argv[1], strerror(errno));
-        return 2;
+        exit(2);
     }
 
     rv = write(fd, argv[2], strlen(argv[2]));
     if (rv < 0)
     {
         printf ("write(): %s\n", strerror(errno));
-        return 3;
+        exit(3);
     }
 
     close(fd);
-    return 0;
+    exit(0);
 }
